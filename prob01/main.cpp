75489bf58d9628532ef3516cd558536ae14aa433
Teresa Shea <45911992+tmshea@users.noreply.github.com>
2019-10-07 02:14:51 -0400

    Update main.cpp

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -35,7 +35,7 @@ int main()
   d2 *= 0.1;
   f2 *= 0.1;
 
-  // TODO#2 add comment describing what's happening
+  // TODO#2 add comment describing why f2 is now zero
   std::cout << "\n**comment**" << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f2 << std::endl;
@@ -43,9 +43,9 @@ int main()
   double d3 = 3.5E38;
   float f3 = 3.5E38;
 
-  // TODO#3 add comment describing what happened
+  // TODO#3 add comment describing why f2 says "inf"
   std::cout << "\n**comment**" << std::endl;
-  std::cout << std::setprecision(1);
+  std::cout << std::setprecision(0);
   std::cout << sizeof(d3) << " bytes, value: " << d3 << std::endl;
   std::cout << sizeof(f2) << " bytes, value: " << f3 << std::endl;
 
